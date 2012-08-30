573e91a7ec4bb10141c64cc7fa4bc6be31c3819e
synthresin <synthresin@me.com>
2012-08-31 00:39:56 +0900

    세션 create 행동 정의 시작

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.authenticate(params[:session][:email],params[:session][:password])
+		
+		if user.nil?
+			render 'new'
+		else
+			# 어드민이면 관리페이지, 업체면 자기 페이지
+			
+		end
 	end
 
 	def destroy
