1283ce687f4945be3a91cacbe8d9bb0a03a7e712
synthresin <synthresin@me.com>
2012-08-30 14:05:34 +0900

    프로젝트 시작

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jfk::Application.config.secret_token = '63b2b6bda29e0fb80c2386ecd5fcd08a2aab173cc2b80bb2232f4a4067357367acbcedc389e43bd1b9dfd0caeba964c6fcd766a35bbaa5fee661981ab6245cfc'
