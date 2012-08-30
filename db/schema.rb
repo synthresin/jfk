06c0f2b45de29ced42784aea5fe6e0fd918ae418
synthresin <synthresin@me.com>
2012-08-30 17:09:46 +0900

    어드민 단 유저(업체들) 컨트롤러 추가

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120830065910) do
+ActiveRecord::Schema.define(:version => 20120830074754) do
+
+  create_table "fairs", :force => true do |t|
+    t.string   "year"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
