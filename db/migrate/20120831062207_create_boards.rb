dcd002049facc7611652520651a0cf267395dd04
synthresin <synthresin@me.com>
2012-08-31 17:45:59 +0900

    보드 마이그레이션 명세서 수정

--- db/migrate/20120831062207_create_boards.rb
+++ db/migrate/20120831062207_create_boards.rb
@@ -1,7 +1,9 @@
 class CreateBoards < ActiveRecord::Migration
   def change
     create_table :boards do |t|
-
+      t.string :title
+      t.integer :post_id
+      
       t.timestamps
     end
   end
