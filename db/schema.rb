7a2b34b09d67e155d7a985ddca1b9c4ef0122095
synthresin <synthresin@me.com>
2012-08-31 18:28:15 +0900

    스키마 변경

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120831062207) do
+ActiveRecord::Schema.define(:version => 20120831092355) do
 
   create_table "boards", :force => true do |t|
     t.string   "title"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(:version => 20120831062207) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "board_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "posts", ["board_id"], :name => "index_posts_on_board_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
