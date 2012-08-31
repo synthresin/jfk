677455ff17f63bbf3b0c574c35a94d6a2d8c5f66
synthresin <synthresin@me.com>
2012-08-31 18:23:37 +0900

    보드 마이그레이션 명세서 ( 이름 뿐)

--- db/migrate/20120831062207_create_boards.rb
+++ db/migrate/20120831062207_create_boards.rb
@@ -2,8 +2,7 @@ class CreateBoards < ActiveRecord::Migration
   def change
     create_table :boards do |t|
       t.string :title
-      t.integer :post_id
-      
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120830074754) do
+ActiveRecord::Schema.define(:version => 20120831062207) do
+
+  create_table "boards", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "fairs", :force => true do |t|
     t.string   "year"
